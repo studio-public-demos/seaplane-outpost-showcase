2446a7f9ab70b8a36ba8eae89ddd4d74511fcb90
gvbenterprises <gvbenterprises.hyd@gmail.com>
2026-07-23 11:18:05 +0530

    Initial commit: Seaplane Outpost Showcase

--- check_db.js
+++
@@ -1,7 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('ddmp.db');
-
-const tables = db.prepare("SELECT name FROM sqlite_master WHERE type='table';").all();
-console.log('Tables:', tables);
-
-db.close();
